df18ab1108e3e6be93feb9e295e57c3f275bae03
BiancaMaciel <bianca_maciel.c@hotmail.com>
2017-11-08 09:43:37 -0200

    connection_database

---
+++ Connect.py
@@ -0,0 +1,56 @@
+import psycopg2
+import os, sys
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+class Connect:
+
+
+    def connection_database(self):
+        conn = psycopg2.connect("user='postgres' password='postgres' host = '127.0.0.1' port= '5432' ")
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+        return cur
+
+    def delete_database(self):
+        try:
+            cur = self.connection_database()
+            drop_database = "DROP DATABASE python"
+            cur.execute(drop_database)
+            cur.close()
+            print("deleted successfully")
+        except Exception as e:
+            print (e)
+            print ("Cannot Delete Database")
+
+    def create_database(self):
+        try:
+            cur = self.connection_database()
+            dbname = "python"
+            cur.execute('CREATE DATABASE ' + dbname)
+            cur.close()
+            self.restore_database()
+        except Exception as e:
+            print (e)
+            print ("Cannot Create Database")
+            try:
+                self.delete_database()
+                self.create_database()
+            except Exception as e:
+                print ("Cannot Delete Database in def create_database")
+
+
+    def restore_database(self):
+        try:
+            cur = self.connection_database()
+            command = "/usr/bin/pg_restore -h 'localhost' -p '5432' -U 'postgres' -d 'python' -v '/home/bianca/unittest_serramar.backup'"
+            os.system(command)
+            cur.close()
+        except Exception as e:
+            print (e)
+            print ("Cannot Restore Database")
+
+if __name__ == '__main__':
+
+    database_conn = Connect()
+    database_conn.create_database()
+    database_conn.delete_database()
